15c374e70b546b13dc7638ca9d89a7306f2702af
Ushinji <siguma971@gmail.com>
2019-11-27 21:17:08 +0900

    Add bootstrap script

---
+++ scripts/development/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo '### start bootstrap ###'
+echo ''
+
+docker-compose build
+docker-compose run --rm app bundle install
+docker-compose run --rm app yarn install
+docker-compose run --rm app bin/rails db:create
+docker-compose run --rm app bin/rails db:migrate
+
+docker-compose up -d
