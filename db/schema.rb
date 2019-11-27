a474619a9f333bba5bbe772e0a48fcd65cfd5550
Ushinji <siguma971@gmail.com>
2019-11-27 21:28:36 +0900

    rails generate scaffold user name:string age:integer  is_active:boolean

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_27_122134) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.boolean "is_active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
